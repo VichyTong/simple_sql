39b28d1c6730b4a6a29141df1a0d012ed635e39f
ted <vichytong@163.com>
2023-05-11 16:13:39 +0800

    [fix] fix app

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 import psycopg2
 from pgvector.psycopg2 import register_vector
+import openai
+import numpy as np
 
 app = Flask(__name__)
 
@@ -14,15 +16,32 @@ DATABASE = {
 }
 
 
+def embedding(words):
+    response = openai.Embedding.create(
+        input=words,
+        engine="text-embedding-ada-002"
+    )
+    vector = np.array(response.data[0].embedding)
+    return vector
+
+
 # API 接口，查询离指定经纬度最近的 N 个结果
 @app.route('/api/get_nearest_N', methods=['POST'])
 def get_nearest_N():
     # 获取请求参数
-    vector = request.json.get('vector')
+    words = request.json.get('words')
     n = request.json.get('n')
 
+    vector = embedding(words)
+
     # 执行 SQL 查询
-    conn = psycopg2.connect(**DATABASE)
+    conn = psycopg2.connect(
+        database='postgres',
+        host='47.93.47.3',
+        user='postgres',
+        password='',
+        port='5433'
+    )
     conn.autocommit = True
     cur = conn.cursor()
     cur.execute("CREATE EXTENSION IF NOT EXISTS vector")
